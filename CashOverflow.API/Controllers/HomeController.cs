f52a5222e574d56a8393813cbbdaff9788128090
Zafar Urakov <goliburakov535@gmail.com>
2023-08-29 19:45:14 +0900

    INFRA: Create New Project

---
+++ CashOverflow.API/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashOverflow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get() => "Cash flows";
+    }
+}
