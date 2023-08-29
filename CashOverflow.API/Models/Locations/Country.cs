49d6d08b09ee44c7a0df84c7cc86e338dd888cc2
Zafar Urakov <goliburakov535@gmail.com>
2023-08-29 19:46:44 +0900

    DATA:Create Model Location

---
+++ CashOverflow.API/Models/Locations/Country.cs
@@ -0,0 +1,11 @@
+﻿// --------------------------------------------------------
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Developed by CashOverflow Team
+// --------------------------------------------------------
+
+namespace CashOverflow.Models.Locations
+{
+    public enum Country
+    {
+        Uzbekistan
+    }
